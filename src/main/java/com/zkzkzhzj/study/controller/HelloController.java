6eb23c3ee809f438d81552131a61dfa1e3dbcfd4
zkzkzhzj <zkzkzhzj@gmail.com>
2023-10-03 20:01:41 +0900

    feat: add template

--- src/main/java/com/zkzkzhzj/study/controller/HelloController.java
+++ src/main/java/com/zkzkzhzj/study/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.zkzkzhzj.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,15 @@ public class HelloController {
 
         // resources -> templates 하단 *.html 에서 * 과 매핑
         return "hello";
+    }
+
+    // 매핑 추가
+    @GetMapping("hello-mvc")
+    // 직접 입력하지 않고 도메인에서 파라매터를 가져온다
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        // 가져온 파라매터 넘겨주기
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
