ad3544f3db7c09eb4c4aaec7700a56733e6a560b
zkzkzhzj <zkzkzhzj@gmail.com>
2023-11-04 18:51:05 +0900

    refactor: 중복회원 검증 로직 리펙토링

--- src/main/java/com/zkzkzhzj/study/service/MemberService.java
+++ src/main/java/com/zkzkzhzj/study/service/MemberService.java
@@ -14,12 +14,16 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+        validateDuplicateMember(member); // 중복 회원 검증
 
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 }
