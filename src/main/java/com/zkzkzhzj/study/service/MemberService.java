b5917ec07f27b79a43847ef18bd4a8f74443bc9d
zkzkzhzj <zkzkzhzj@gmail.com>
2023-11-04 18:54:31 +0900

    feat: 전체회원 조회 및 개별 조회 추가

--- src/main/java/com/zkzkzhzj/study/service/MemberService.java
+++ src/main/java/com/zkzkzhzj/study/service/MemberService.java
@@ -4,6 +4,7 @@ import com.zkzkzhzj.study.domain.Member;
 import com.zkzkzhzj.study.repository.MemberRepository;
 import com.zkzkzhzj.study.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -11,7 +12,7 @@ public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     /**
-     * 회원가입
+     * 회원 가입
      */
     public Long join(Member member) {
         validateDuplicateMember(member); // 중복 회원 검증
@@ -25,5 +26,16 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
