1b304d432d0abcb9cebed5d91a11f63f6a983dc4
zkzkzhzj <zkzkzhzj@gmail.com>
2023-11-14 23:33:50 +0900

    feat: jdbc 연동

--- src/main/java/com/zkzkzhzj/study/SpringConfig.java
+++ src/main/java/com/zkzkzhzj/study/SpringConfig.java
@@ -1,14 +1,21 @@
 package com.zkzkzhzj.study;
 
+import com.zkzkzhzj.study.repository.JdbcMemberRepository;
 import com.zkzkzhzj.study.repository.MemberRepository;
 import com.zkzkzhzj.study.repository.MemoryMemberRepository;
 import com.zkzkzhzj.study.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 // 직접 스프링 빈을 등록 하기
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // 스프링 빈에 등록 되어 멤버 서비스에 등록
     @Bean
@@ -18,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
