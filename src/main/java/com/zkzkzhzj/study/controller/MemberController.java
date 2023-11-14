91ddae79881c9268ba5f73f44ee0468c431ed693
zkzkzhzj <zkzkzhzj@gmail.com>
2023-11-14 23:13:31 +0900

    feat: mvc 패턴 도입, 가입 및 목록 리스트 구현

--- src/main/java/com/zkzkzhzj/study/controller/MemberController.java
+++ src/main/java/com/zkzkzhzj/study/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.zkzkzhzj.study.controller;
 
+import com.zkzkzhzj.study.domain.Member;
 import com.zkzkzhzj.study.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // spring bean 등록, 컨트롤러로 가지고 있음
 @Controller
@@ -14,5 +20,25 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
